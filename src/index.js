6143727154e4780405553cc18e843dcd2c5fed72
EvanE1233 <evan.eachus@student.swic.edu>
2025-03-06 12:59:07 -0600

    added demo code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-var x = 3;
-
-console.log(x);
+const x = 3
+const y = [1, 2, 3]
+const z = 'a word'
+const c = {
+  house: 'Big',
+  mouse: 'small',
+}
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(c)
+console.log(c.house)
+console.log(x + y)
