5460779040be44b97c9b36aaa651a66070627edd
EvanE1233 <evan.eachus@student.swic.edu>
2025-03-06 13:02:36 -0600

    add string formatting code

---
+++ src/strings.js
@@ -0,0 +1,7 @@
+const name = 'Evan Eachus'
+const course_name = 'Web Devolopment'
+const expected_grade = 85
+const status = name + ' In ' + course_name + ', striving to get at least a ' + expected_grade + '%'
+const better_status = `${name} in ${course_name}, striving to get at least a ${expected_grade}%`
+console.log(status)
+console.log(better_status)
